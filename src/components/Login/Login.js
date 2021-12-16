94e065e8a2d96960307e8cc0dda04623ec3f2f3c
Damian <you@example.com>
2021-12-16 20:48:58 +0100

    Added navigation arrow

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { PostData } from "../../services/PostData";
 import BookIco from "@mui/icons-material/LocalLibraryOutlined";
+import ArrowIco from '@mui/icons-material/ArrowBack';
 import { Navigate } from "react-router-dom";
 
 const Login = () => {
@@ -39,14 +40,20 @@ const Login = () => {
   return (
     <div class="container mx-auto">
       <div className="row position-absolute top-50 start-50 translate-middle">
-        <div className="border rounded  text-center bg-light">
-          <BookIco className="mt-5" fontSize="large" />
-
-          <div className="row mx-3 mt-4 ">
+        <div className="border rounded  bg-light col-12">
+          <div className="">
+            <a href="/" style = {{color:"black"}}>
+              <ArrowIco className="mt-3 mx-1" fontSize="large" />
+            </a>
+          </div>
+          <div className="text-center">
+            <BookIco className="" fontSize="large" />
+          </div>
+          <div className="row mx-3 mt-4 text-center">
             <h6 for="exampleInputEmail1">Enter Email address</h6>
             <input
               type="email"
-              name = "email"
+              name="email"
               className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
@@ -55,25 +62,26 @@ const Login = () => {
             />
 
           </div>
-          <div className="row mx-3 mt-4 ">
+          <div className="row mx-3 mt-4 text-center">
             <h6 for="exampleInputEmail1">Enter password </h6>
             <input
               type="password"
-              name = "password"
+              name="password"
               className="form-control"
               id="exampleInputEmail1"
               placeholder="Enter email"
               onChange={handleChange}
             />
           </div>
-
-          <a
-            type="button"
-            onClick={login}
-            className="mx-1 mt-4 fs-5 mb-4 p-2 btn btn-primary"
-          >
-            Login
-          </a>
+          <div className="text-center">
+            <a
+              type="button"
+              onClick={login}
+              className="mx-1 mt-4 fs-5 mb-4 p-2 btn btn-primary"
+            >
+              Login
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Navigate } from "react-router";
 import { PostData } from "../../services/PostData";
 import BookIco from "@mui/icons-material/LocalLibraryOutlined";
+import ArrowIco from '@mui/icons-material/ArrowBack';
 
 const Footer = () => {
   //inputs should be passed to PostData (fetch)
@@ -28,7 +29,7 @@ const Footer = () => {
       }
     });
   };
-  
+
 
   if (success) {
     return <Navigate to={"/"} />;
@@ -41,59 +42,67 @@ const Footer = () => {
   return (
     <div class="container mx-auto">
       <div className="row position-absolute top-50 start-50 translate-middle">
-        <div className="border rounded  text-center bg-light">
-          <BookIco className="mt-5" fontSize="large" />
+        <div className="border rounded bg-light">
+          <div className="">
+            <a href="/" style={{ color: "black" }}>
+              <ArrowIco className="mt-3 mx-1" fontSize="large" />
+            </a>
+          </div>
+          <div className=" text-center ">
+            <BookIco className="mt-1" fontSize="large" />
+          </div>
 
-          <div className="row mx-3 mt-3 ">
+          <div className="row mx-3  text-center  mt-3 ">
             <h6 >Enter your first name</h6>
             <input
-              name = "firstName"
+              name="firstName"
               className="form-control"
               aria-describedby="emailHelp"
               placeholder="First name"
               onChange={handleChange}
             />
           </div>
-          <div className="row mx-3 mt-3 ">
+          <div className="row mx-3 mt-3  text-center ">
             <h6 >Enter your second name</h6>
             <input
-              name = "secondName"
+              name="secondName"
               className="form-control"
               aria-describedby="emailHelp"
               placeholder="Second name"
               onChange={handleChange}
             />
           </div>
-          <div className="row mx-3 mt-3 ">
+          <div className="row mx-3 mt-3  text-center ">
             <h6 >Enter your email</h6>
             <input
-              name = "email"
+              name="email"
               className="form-control"
-              type = "email"
+              type="email"
               aria-describedby="emailHelp"
               placeholder="email"
               onChange={handleChange}
             />
 
           </div>
-          <div className="row mx-3 mt-3 ">
+          <div className="row mx-3 mt-3  text-center ">
             <h6 >Enter password </h6>
             <input
               type="password"
-              name = "password"
+              name="password"
               className="form-control"
               placeholder="password"
               onChange={handleChange}
             />
           </div>
-
-          <a
-            onClick={signup}
-            type="button"
-            className="mx-1 mt-4 fs-5 mb-4 p-2 btn btn-primary"
-          >
-            Signup
-          </a>
+          <div className="text-center">
+            <a
+              onClick={signup}
+              type="button"
+              className="mx-1 mt-4 fs-5 mb-4 p-2 btn btn-primary"
+            >
+              Signup
+            </a>
+          </div>
         </div>
       </div>
     </div>
